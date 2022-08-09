3ce59fa2661c3c32bc7a02d795b5f04605ac38ac
Genielson <ge.nielson@hotmail.com>
2022-08-09 19:15:19 -0300

    feat / Adicionando model,seeder,factory e controller de Post

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Post;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -33,6 +33,9 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+
+
+
     /**
      * The attributes that should be cast.
      *
@@ -41,4 +44,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function users(){
+        return $this->hasMany(Post::class,'user_id');
+    }
 }
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'description' => $this->faker->text,
+            'user_id' => '1'
+        ];
+    }
+}
