ae1908e489b7c34024c1d5d8a2e3022f7e605737
Genielson <ge.nielson@hotmail.com>
2022-08-29 20:00:44 -0300

    feat(view) / Adicionando nova view para criar um novo post

---
+++ resources/views/post-create.blade.php
@@ -0,0 +1,60 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+@stop
+
+@section('content')
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+
+                        <div class="card-header">
+                            <h3 class="card-title">Novo Post </h3>
+                        </div>
+
+
+                        <form method="post" action="" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <textarea class="ckeditor form-control" name="wysiwyg-editor"></textarea>
+                            </div>
+                        </form>
+
+
+                    </div>
+
+                </div>
+
+
+                <div class="col-md-6">
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+    <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.ckeditor').ckeditor();
+        });
+    </script>
+@stop
+
+
