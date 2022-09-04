3b3df840aa9185ecd6d867c7d117b22d632010b0
Genielson <ge.nielson@hotmail.com>
2022-09-04 16:20:44 -0300

    feat/ Adicionando middleware para LogAccess

--- app/Models/LogAccessUser.php
+++ app/Models/LogAccessUser.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class LogAccess extends Model
+class LogAccessUser extends Model
 {
     use HasFactory;
     protected $fillable = ['log'];
