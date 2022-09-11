a9048048b6241e31d2d5076cc77f7c5f44fbeece
Genielson <ge.nielson@hotmail.com>
2022-09-11 13:21:46 -0300

    feat/ Adicionando models e migrations de Site

---
+++ database/migrations/2022_09_11_153701_create_sites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url_image_logo');
+            $table->string('header_background');
+            $table->string('footer_background');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
