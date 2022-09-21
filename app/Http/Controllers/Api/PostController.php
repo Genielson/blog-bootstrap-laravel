0a528bd136182f18b90e4fb8fe2c66482e64522b
Genielson <ge.nielson@hotmail.com>
2022-09-21 19:27:49 -0300

    feat/ Adicionando funcionalidade na API de deletar um Post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PostCategory;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PostController extends Controller
 {
@@ -22,4 +24,67 @@ class PostController extends Controller
         $posts = Post::all();
         return response()->json($posts);
     }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if($id == NULL){
+            return response()->json([
+                'message' => 'O id deve ser diferente de zero'
+            ],206);
+        }
+        $post = Post::findOrFail($id);
+        if($post == NULL){
+            return response()->json([
+                'message' => 'Post não encontrado',
+                'success' => false
+            ],404);
+        }
+        if($post->delete()){
+            $postCategory = PostCategory::where('post_id','=',$id);
+            $postCategory->delete();
+            return response()->json([
+                'message' => 'Post deletado com sucesso',
+                'success' => true
+            ],204);
+        }else{
+            return response()->json([
+                'message' => 'Usuario não pode ser deletado, tente mais tarde',
+                'success' => false
+            ],204);
+        }
+
+    }
+
+
 }
