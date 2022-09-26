4063ba3e80c09a8469c7c1f047097fbe4d529af3
Genielson <ge.nielson@hotmail.com>
2022-09-26 19:40:42 -0300

    feat/ Adicionando classe Request UpdateSiteRequest para validacao de inputs

--- app/Http/Requests/UpdateSiteRequest.php
+++ app/Http/Requests/UpdateSiteRequest.php
@@ -16,15 +16,30 @@ class UpdateSiteRequest extends FormRequest
         return false;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
+
     public function rules()
     {
         return [
-            //
+            'title'=>'required|min:1|max:100',
+            'header'=>'required',
+            'footer'=>'required',
+            'image'=>'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'title.required' => ' O titulo não pode estar em branco',
+            'title.min' => 'O titulo deve ter ao menos 1 caractere' ,
+            'title.max' => 'O titulo deve ter no máximo 100 caracteres',
+            'image.required' => ' É obrigatório inserir uma imagem ',
+            'image.image' => 'O upload tem que ser do tipo imagem',
+            'image.mimes' => ' O formato de envio deve ser pg,png,jpeg,gif,svg ',
+            'image.max' => ' O tamanho máximo para upload de imagem é 2MB',
+            'header.required' => ' É necessario escolher uma cor para o header ',
+            'footer.required' => ' É necessario escolher uma cor para o footer '
+        ];
+    }
+
 }
